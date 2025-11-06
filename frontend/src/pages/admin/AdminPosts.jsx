ef5f5dc30176fdcf8cb49413090b115e37981b47
hslee15 <lhs01019@kbu.ac.kr>
2025-11-06 17:26:10 +0900

    1

--- frontend/src/pages/admin/AdminPosts.jsx
+++ frontend/src/pages/admin/AdminPosts.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React ,{useEffect, useState}from 'react'
+import { feetchAdminPosts, patchAdminPost } from '../../api/AdminApi'
+import AdminFilter from '../../components/admin/AdminFilter'
+import AdminPostsList from '../../components/admin/AdminPostsList'
+
 
 const AdminPosts = () => {
+  const [list, setList]=useState([])
+
+  const [query, setQuery]=useState({
+    page:1,
+    size:10,
+    status:'',
+    q:'',
+    user:''
+  })
+
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const items=await feetchAdminPosts(query)
+        setList(items)
+      } catch (error) {
+        console.error('게시글 불러오기 실패',error)
+      }
+    })
+  },[query])
+
+
   return (
-    <div>AdminPosts</div>
+    <div>
+      <AdminFilter/>
+      <AdminPostsList items={list}/>
+    </div>
   )
 }
 
